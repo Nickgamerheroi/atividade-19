d6ddfb1b4434dc84ee8cd32a9cbeefe3c6ef58f8
Nickgamerheroi <164543016+Nickgamerheroi@users.noreply.github.com>
2025-03-13 15:51:50 +0000

    novo

---
+++ js/login.js
@@ -0,0 +1,41 @@
+const botaoLogin = document.querySelector('#bt-entrar');
+botaoLogin.addEventListener('click', autenticar);
+
+const areaMensagem = document.getElementById('msg');
+
+async function autenticar(e) {
+  e.preventDefault(); 
+
+  document.getElementById('msg').innerText = "Aguarde... ";
+
+  const dados = {
+    email: document.getElementById('email').value,
+    senha: document.getElementById('senha').value
+  };
+
+  const url = "https://13-web.vercel.app/login";
+
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json' 
+      },
+      body: JSON.stringify(dados)
+    });
+
+    if (!response.ok) {
+      throw new Error("Email/Senha incorretos! - " + response.status);
+    }
+
+    const data = await response.json();
+
+    localStorage.setItem('jwt', data.token);
+
+    window.location.href = 'login.html';
+
+  } catch (error) {
+    areaMensagem.style = "color:red";
+    areaMensagem.innerHTML = error;
+  }
+}
